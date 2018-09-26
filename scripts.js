aebe8b64362719530f4dea1ac5356361bd1e5e85
Gama Suárez <gamalielsuarez5@gmail.com>
2018-09-25 19:49:06 -0500

    inicio de ejercicios de js

---
+++ scripts.js
@@ -0,0 +1,70 @@
+
+// var nombre = "Gama";
+// var apellido = "Suarez";
+// varnombre_completo = nombre + " " + apellido;
+//
+// var edad = 21;
+// var casadi = true;
+//
+// consol.log("Mi nombre es " + nombre);
+// consol.log (edad)
+// consol.log(casado)
+
+// Horas de sueño
+// let dormir = Number(prompt("A que hora dormiste"));
+// let despertar = Number(prompt("A que hora despertaste"));
+//
+// if(dormir <=12) {
+//     var horas dormidas_ = despertar + (12-dormir);
+// } else {
+//   var horas_dormidas = despertar + (24-dormir);
+// }
+// console.log(horas_dormidas);
+//
+// //Ciclos - Regaño
+// for(let contador = 0; contador <= 100; contador+=5) {
+//   console.log(contador);
+// }
+
+//Saludo
+// var nombre = "Gama"; //String
+// console.log(nombre);
+//
+// if(nombre =="Gama") {
+//   console.log("Hola Gama");
+// } else if(nombre == "AlexisDoli") {
+//   console.log("Hola AlexisDoli")
+// } else {
+//   console.log("Tu no eres Gama ni AlexisDoli")
+// }
+
+
+// // Lista del super
+// let Sal = Number(prompt("Cuanto cuesta la sal"));
+// let Arroz = Number(prompt("Cuanto cuesta el arroz"));
+// let Cereal = Number(prompt("Cuanto cuesta el cereal"));
+// let Azucar = Number(prompt("cuanto cuesta el azucar"));
+// let Helado = Number(prompt("Cuanto cuesta el helado"));
+//
+// var Total_supermercado = Sal + Arroz + Cereal + Azucar + Helado
+//
+// console.log("El total de tu super es $" + Total_supermercado)
+
+
+// // Validaciones Felicidad o hambre
+// let hambre = prompt("tienes hambre? (si, no)")
+// if (hambre == "si") {
+//   console.log("Estas triste")
+// } else if (hambre == "no") {
+//   console.log("Estas Feliz")
+// }
+
+// Peso ideal Ciclos
+let peso_ideal = (prompt("¿Cual es tu peso ideal?"));
+let peso_actual = (prompt("¿Cuanto pesas actualmente?"));
+let actividades = (prompt("¿Quieres saber que tienes que hacer para llegar a tu peso ideal?"));
+  if (actividades == "si") {
+  console.log("Corre 30 min todos los dias de 9 a 9:30, come saludable por 3 meses y has ejercicio todos los dias minimo 1 hora")
+}  else if (actividades == "no") {
+  console.log("entonces sigue comiento hamburguesas y malteadas")
+}
