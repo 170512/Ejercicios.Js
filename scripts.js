36c8ed2c3bb78a0514acf7d258e70a55e3f3ba8a
Gama Suárez <gamalielsuarez5@gmail.com>
2018-09-26 22:02:25 -0500

    Terminamos el piedra papel o tijeras e hicimos otro de 2 jugadores :3

--- scripts.js
+++ scripts.js
@@ -10,23 +10,7 @@
 // consol.log (edad)
 // consol.log(casado)
 
-// Horas_de_sueño
-// let dormir = Number(prompt("A que hora dormiste"));
-// let despertar = Number(prompt("A que hora despertaste"));
-//
-// if(dormir <=12) {
-//     var horas dormidas_ = despertar + (12-dormir);
-// } else {
-//   var horas_dormidas = despertar + (24-dormir);
-// }
-// console.log(horas_dormidas);
-
-
-// Regaño
-//Ciclos
-// for(let contador = 0; contador <= 100; contador+=5) {
-//   console.log(contador);
-// }
+// ----------------------------------------------------------------------------------------
 
 //Saludo
 // var nombre = "Gama"; //String
@@ -40,6 +24,7 @@
 //   console.log("Tu no eres Gama ni AlexisDoli")
 // }
 
+// -----------------------------------------------------------------------------------------
 
 // // Lista del super
 function lista_del_super () {
@@ -54,6 +39,8 @@ var Total_supermercado = Sal + Arroz + Cereal + Azucar + Helado
 console.log("El total de tu super es $" + Total_supermercado)
 }
 
+// -----------------------------------------------------------------------------------------
+
 // // Validaciones Felicidad o hambre
 // let hambre = prompt("tienes hambre? (si, no)")
 // if (hambre == "si") {
@@ -61,6 +48,8 @@ console.log("El total de tu super es $" + Total_supermercado)
 // } else if (hambre == "no") {
 //   console.log("Estas Feliz")
 // }
+
+// ------------------------------------------------------------------------------------------
 
 // Peso ideal
 // Ciclos
@@ -80,6 +69,8 @@ function peso_ideal () {
   }
 }
 
+// -------------------------------------------------------------------------------------------
+
 //declarando la funcion
 function Horas_de_sueño () {
   let dormir = Number(prompt("A que hora dormiste"));
@@ -94,6 +85,7 @@ function Horas_de_sueño () {
 }
 // Ejecutar la funcion
 
+// --------------------------------------------------------------------------------------------------
 
 // Regaño
 // Declarar la funcion
@@ -104,16 +96,82 @@ function Regaño () {
   console.log("En total Paco escribio del 0 al 100 de 5 en 5.")
 }
 
+// -------------------------------------------------------------------------------------------
 
 function compu() {
-  // let tiro = Math.random() * 2
-  let mi_tiro = prompt("Te toca tirar. (recuerda que 0 es piedra, 1 papel y 2 tijeras)")
-  let tiro = Math.floor(Math.random() * 3); // Numero random entre 0 y 2
-  console.log(tiro);
+  let tiro_compu = Math.floor(Math.random() * 3);
+
+  if (tiro_compu == 0) {
+      tiro_compu = "piedra";
+  }
+  if (tiro_compu == 1) {
+      tiro_compu = "papel";
+  }
+  if (tiro_compu == 2) {
+      tiro_compu = "tijeras";
+  }
+  let mi_tiro = prompt("¿piedra, papel o tijeras?")
+
+// EMPATE
+  if (mi_tiro == tiro_compu) {
+    console.log("Mi tiro es: " + mi_tiro + ", Compu tiro: " + tiro_compu + ": Es un empate")
+  }
+
+// GANASTE!!!!
+  if (mi_tiro == "piedra" && tiro_compu == "tijeras") {
+    console.log("Mi tiro es: " + mi_tiro + ", Compu tiro: " + tiro_compu + ": GANASTE!!!!!")
+  } else if (mi_tiro == "papel" && tiro_compu == "piedra") {
+    console.log("Mi tiro es: " + mi_tiro + ", Compu tiro: " + tiro_compu + ": GANASTE!!!!!")
+  } else if (mi_tiro == "tijeras" && tiro_compu == "papel"){
+    console.log("Mi tiro es: " + mi_tiro + ", Compu tiro: " + tiro_compu + ": GANASTE!!!!!")
+  }
+
+// PERDISTE
+  if (mi_tiro == "piedra" && tiro_compu == "papel") {
+    console.log("Mi tiro es: " + mi_tiro + ", Compu tiro: " + tiro_compu + ": GANO LA COMPU")
+  } else if (mi_tiro == "papel" && tiro_compu == "tijeras") {
+    console.log("Mi tiro es: " + mi_tiro + ", Compu tiro: " + tiro_compu + ": GANO LA COMPU")
+  } else if (mi_tiro == "tijeras" && tiro_compu == "piedra") {
+    console.log("Mi tiro es: " + mi_tiro + ", Compu tiro: " + tiro_compu + ": GANO LA COMPU")
+  }
 }
 
+// ---------------------------------------------------------------------------------------------------
 
-// Math.random(); // Numero random entre el 0 y el 1 (con decimales)
-// Math.round(); // Redondea arriba y abajo
-// Math.floor(); // Redondea hacia abajo
-// Math.ceil(); // Reondea havia arriba
+function two_players() {
+
+  let tiro_1P = prompt("¿piedra, papel o tijeras? Jugador 1")
+  let tiro_2P = prompt("¿piedra, papel o tijeras? Jugador 2")
+
+// EMPATE
+  if (tiro_1P == tiro_2P) {
+    console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": Es un empate, Intenta otra vez")
+  }
+
+// GANASTE!!!! J1
+  if (tiro_1P == "piedra" && tiro_2P == "tijeras") {
+    console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": Gano J1")
+  } else if (tiro_1P == "papel" && tiro_2P == "piedra") {
+    console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": Gano J1")
+  } else if (tiro_1P == "tijeras" && tiro_2P == "papel") {
+    console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": Gano J1")
+  }
+
+  // GANASTE!!!! J2
+    if (tiro_2P == "piedra" && tiro_1P == "tijeras") {
+      console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": Gano J2")
+    } else if (tiro_2P == "papel" && tiro_1P == "piedra") {
+      console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": Gano J2")
+    } else if (tiro_2P == "tijeras" && tiro_1P == "papel") {
+      console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": Gano J2")
+    }
+}
+
+// // PERDISTE
+//   if (tiro_1P == "piedra" && tiro_2P == "papel") {
+//     console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": ")
+//   } else if (tiro_1P == "papel" && tiro_2P == "tijeras") {
+//     console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": ")
+//   } else if (tiro_1P == "tijeras" && tiro_2P == "piedra") {
+//     console.log("tiro_1P: " + tiro_1P + ", tiro_2p: " + tiro_2P + ": ")
+//   }
